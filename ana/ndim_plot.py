576050efcd6a1d3d88cfb8615e6cbc690f66a51f
pSpitzner <paul.spitzner@ds.mpg.de>
2021-05-27 12:00:02 +0200

    plotting lines with errors

--- ana/ndim_plot.py
+++ ana/ndim_plot.py
@@ -2,7 +2,7 @@
 # @Author:        F. Paul Spitzner
 # @Email:         paul.spitzner@ds.mpg.de
 # @Created:       2020-07-16 11:54:20
-# @Last Modified: 2021-05-10 16:22:14
+# @Last Modified: 2021-05-27 11:58:53
 #
 # plot a merged down, multidimensional hdf5 file (from individual simulations)
 # and select which dims to show where
@@ -50,11 +50,10 @@ parser.add_argument(
     "-c", "--center", dest="center_cmap_around", default=None, type=float,
 )
 parser.add_argument(
-    "-a",
-    "--annot",
-    dest="enable_annotation",
-    default=False,
-    action="store_true",
+    "-a", "--annot", dest="enable_annotation", default=False, action="store_true",
+)
+parser.add_argument(
+    "-l", "--line", dest="enable_lineplot", default=False, action="store_true",
 )
 
 args = parser.parse_args()
@@ -77,6 +76,14 @@ l_obs_candidates = h5.ls(input_path, "/data/")
 l_obs_candidates = [obs for obs in l_obs_candidates if obs.find("axis_") != 0]
 assert len(l_obs_candidates) > 0
 
+def isint(value):
+  try:
+    int(value)
+    return True
+  except ValueError:
+    return False
+
+
 options = f""
 for idx, opt in enumerate(l_obs_candidates):
     options += f"{idx:d} {opt}\n"
@@ -85,12 +92,14 @@ while True:
     txt = input(f"Choose observable to plot, e.g. '1'\n{options}> ")
     if len(txt) == 0:
         obs_to_plot = l_obs_candidates[0]
+    elif not isint(txt):
+        continue
+    elif int(txt) >= len(l_obs_candidates):
+        continue
     else:
-        if int(txt) >= len(l_obs_candidates):
-            continue
         obs_to_plot = l_obs_candidates[int(txt)]
-        print(f"Using {obs_to_plot}")
-        break
+    print(f"Using {obs_to_plot}")
+    break
 
 data_nd = h5.load(input_path, f"/data/{obs_to_plot}", silent=True)
 
@@ -107,15 +116,15 @@ options = f""
 for idx, opt in enumerate(l_axis_candidates):
     options += f"{idx:d} {opt}\n"
 while True:
-    txt = input(f"Choose 2 axis to plot, e.g. '1 3'\n{options}> ")
+    txt = input(f"Choose x and y axis to plot, e.g. '1 3'\n{options}> ")
     if len(txt) == 0:
         l_axis_selected = list(l_axis_candidates[:2])
     else:
         txt = txt.split(" ")
         if (
             len(txt) < 2
-            or int(txt[0]) > len(l_axis_candidates)
-            or int(txt[1]) > len(l_axis_candidates)
+            or np.any([not isint(i) for i in txt])
+            or np.any([int(i) > len(l_axis_candidates) for i in txt])
         ):
             continue
         l_axis_selected = l_axis_candidates[[int(txt[0]), int(txt[1])]].tolist()
@@ -164,33 +173,66 @@ for k in sorted(ax_idx, reverse=True):
 if l_axis_selected != [i for i in l_axis_candidates if i in l_axis_selected]:
     data_3d = np.swapaxes(data_3d, 0, 1)
 
+x_obs = l_axis_selected[0]
+y_obs = l_axis_selected[1]
+
+plt.ion()
+
+# if enable errorbars, we plot lines
+if args.enable_lineplot:
+    fig, ax = plt.subplots(figsize=(10, 4))
+    data_mean_1d = np.nanmean(data_3d, axis=2)
+    data_std_1d = np.nanstd(data_3d, axis=2)
+    # draw every `y axis` (what would be heatmap y axis) as a new line
+    for ydx, y in enumerate(d_axes[y_obs]):
+        ax.errorbar(
+            x=d_axes[x_obs],
+            y=data_mean_1d[:, ydx],
+            yerr=data_std_1d[:, ydx],
+            fmt="-",
+            markersize=1,
+            # color="C0",
+            # ecolor="C0",
+            # alpha=.75,
+            elinewidth=.5,
+            capsize=3,
+            zorder=0,
+            label=f"{y_obs} = {y:g}"
+        )
+    ax.set_xlabel(x_obs)
+    ax.set_ylabel(obs_to_plot)
+    ax.legend()
+    ax_1d = ax
+
+fig, ax = plt.subplots(figsize=(10, 4))
+
+# otherwise, we create a 2d heatmap
 # plot using seaborn and pandas DataFrame (its just more convenient than manually)
-y_obs = l_axis_selected[0]
-x_obs = l_axis_selected[1]
 data_mean = pd.DataFrame(
     # average across repetitions, which are last axis
     np.nanmean(data_3d, axis=2),
-    index=d_axes[y_obs] if isinstance(d_axes[y_obs], np.ndarray) else [d_axes[y_obs]],
-    columns=d_axes[x_obs] if isinstance(d_axes[x_obs], np.ndarray) else [d_axes[x_obs]],
+    index=d_axes[y_obs]
+    if isinstance(d_axes[y_obs], np.ndarray)
+    else [d_axes[y_obs]],
+    columns=d_axes[x_obs]
+    if isinstance(d_axes[x_obs], np.ndarray)
+    else [d_axes[x_obs]],
 )
-
-plt.ion()
-fig, ax = plt.subplots(figsize=(10, 4))
 
 if args.center_cmap_around is None:
     kwargs = {
-        'vmin' : np.nanmin(data_mean[np.isfinite(data_mean)]),
-        'vmax' : np.nanmax(data_mean[np.isfinite(data_mean)]),
+        "vmin": np.nanmin(data_mean[np.isfinite(data_mean)]),
+        "vmax": np.nanmax(data_mean[np.isfinite(data_mean)]),
         # 'vmin': 0,
         # 'vmax': 150,
-        'cmap': "Blues",
+        "cmap": "Blues",
     }
 else:
     kwargs = {
-        'vmin': 0,
-        'vmax': args.center_cmap_around * 2,
-        'center': args.center_cmap_around,
-        'cmap': "twilight",
+        "vmin": 0,
+        "vmax": args.center_cmap_around * 2,
+        "center": args.center_cmap_around,
+        "cmap": "twilight",
     }
 
 sns.heatmap(
