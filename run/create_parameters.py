300b6c725f8fc250736244087576e8e492fb8276
pSpitzner <paul.spitzner@ds.mpg.de>
2021-06-08 15:29:34 +0200

    stimulation with some inhibition, finally.

--- run/create_parameters.py
+++ run/create_parameters.py
@@ -11,14 +11,17 @@ seed = 3_000
 # parameters to scan, noise rate, ampa strength, and a few repetitons for statistics
 l_topo = ["2x2_fixed"]
 l_k_inter = np.array([1, 2, 3, 5])
-l_mod = np.array(["off", "0", "02", "012", "0123"])
-l_rep = range(0, 25)
+l_mod = np.array(["off", "02", "0123"])
+l_rep = range(0, 200)
 
 bridge_weight = 1.0
 inh_frac = 0.20
-# without inhibition we had around gA 35mV, at this value, the activity is a bit lower
-# lets compensate somewhat
-gA = 39
+
+jA = 45.0
+jG = 67.5
+jM = 25
+rate = 38
+
 
 arg_list = product(l_topo, l_k_inter, l_rep)
 
@@ -36,7 +39,7 @@ with open("./parameters_topo.tsv", "w") as f_topo:
             k_inter = i[1]
             rep = i[2]
 
-            topo_path = f"./dat/inhibition/topo/k={k_inter:d}_bw={bridge_weight:03.2f}_inh={inh_frac:03.2f}_gA={gA:.1f}_rep={rep:02d}.hdf5"
+            topo_path = f"./dat/inhibition/topo/k={k_inter:d}_bw={bridge_weight:03.2f}_inh={inh_frac:03.2f}_jA={jA:.1f}_jG={jG:.1f}_rep={rep:02d}.hdf5"
             f_topo.write(
                 # topology command
                 f"/data.nst/share/projects/paul_brian_modular_cultures/topology_orlandi_standalone/exe/orlandi_standalone "
@@ -46,7 +49,7 @@ with open("./parameters_topo.tsv", "w") as f_topo:
             count_topo += 1
 
             for mod in l_mod:
-                dyn_path = f"./dat/inhibition/dyn/k={k_inter:d}_stim={mod}_bw={bridge_weight:03.2f}_inh={inh_frac:03.2f}_gA={gA:.1f}_rep={rep:02d}.hdf5"
+                dyn_path = f"./dat/inhibition/dyn/k={k_inter:d}_stim={mod}_bw={bridge_weight:03.2f}_inh={inh_frac:03.2f}_jA={jA:.1f}_jG={jG:.1f}_rep={rep:02d}.hdf5"
 
                 if mod == "off":
                     stim_arg = ""
@@ -57,9 +60,10 @@ with open("./parameters_topo.tsv", "w") as f_topo:
                     # dynamic command
                     f"python ./src/quadratic_integrate_and_fire.py -i {topo_path} "
                     + f"-o {dyn_path} "
-                    + f"-d 10800 -equil 300 -s {seed:d} "
+                    + f"-d 3600 -equil 300 -s {seed:d} "
                     + f"--bridge_weight {bridge_weight} "
-                    + f"--inhibition {inh_frac} -gA {gA} "
+                    + f"--inhibition {inh_frac} "
+                    + f"-jA {jA} -jG {jG} -jM {jM} -r {rate} "
                     + f"{stim_arg}\n"
                 )
 
