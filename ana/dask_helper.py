f2fe52dba2ae09d1bd69686edabb94b4c5e68266
pSpitzner <paul.spitzner@ds.mpg.de>
2021-07-14 11:52:19 +0200

    init dask client

--- ana/dask_helper.py
+++ ana/dask_helper.py
@@ -2,7 +2,7 @@
 # @Author:        F. Paul Spitzner
 # @Email:         paul.spitzner@ds.mpg.de
 # @Created:       2021-06-24 18:23:02
-# @Last Modified: 2021-07-14 11:44:21
+# @Last Modified: 2021-07-14 11:51:59
 # ------------------------------------------------------------------------------ #
 
 import os
@@ -43,11 +43,12 @@ def init_dask(n_workers = 256):
             log_directory="./log/dask/",
             local_directory="/scratch01.local/pspitzner/dask/",
             interface="ib0",
-            n_workers=n_workers,
             extra=[
                 '--preload \'import sys; sys.path.append("./ana/"); sys.path.append("/home/pspitzner/code/pyhelpers/");\''
             ],
         )
+        cluster.scale(cores=n_workers)
+
     elif "tahmineh" in os.uname().nodename:
         cluster = SGECluster(
             cores=32,
@@ -57,11 +58,12 @@ def init_dask(n_workers = 256):
             log_directory="/scratch01.local/pspitzner/dask/logs",
             local_directory="/scratch01.local/pspitzner/dask/scratch",
             interface="ib0",
-            n_workers=n_workers,
             extra=[
                 '--preload \'import sys; sys.path.append("./ana/"); sys.path.append("/home/pspitzner/code/pyhelpers/");\''
             ],
         )
+        cluster.scale(cores=n_workers)
+
     elif "rudabeh" in os.uname().nodename:
         cluster = SGECluster(
             cores=32,
@@ -71,16 +73,16 @@ def init_dask(n_workers = 256):
             log_directory="/scratch01.local/pspitzner/dask/logs",
             local_directory="/scratch01.local/pspitzner/dask/scratch",
             interface="ib0",
-            n_workers=n_workers,
             extra=[
                 '--preload \'import sys; sys.path.append("./ana/"); sys.path.append("/home/pspitzner/code/pyhelpers/");\''
             ],
         )
+        cluster.scale(cores=n_workers)
 
     else:
         cluster = LocalCluster(local_directory=f"{tempfile.gettempdir()}/dask/")
-        client = Client(cluster)
 
+    client = Client(cluster)
     log.info(cluster)
     log.info(client)
 
