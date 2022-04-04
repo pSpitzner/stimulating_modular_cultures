de56d5921ff2d8dbfae873971b721e6de7b55798
pSpitzner <paul.spitzner@ds.mpg.de>
2022-04-04 19:02:31 +0200

    numba for mesoscopic model

--- src/mesoscopic_model.py
+++ src/mesoscopic_model.py
@@ -7,31 +7,68 @@ from genericpath import exists
 import numpy as np
 import pandas as pd
 import os
-from numba import jit
 
+import logging
+import warnings
 
-def gate_deactivation_function(src_resources):
-    """
-    Auxiliar implementation of the response function of the gate to the number of resources. For plotting purposes only.
+logging.basicConfig(level=logging.INFO, format="%(levelname)-8s [%(name)s] %(message)s")
+log = logging.getLogger(__name__)
+warnings.filterwarnings("ignore")  # suppress numpy warnings
 
-    #Parameters:
-    src_resources: ndarray
-        Returns the response function of the given array
-    """
+try:
+    from numba import jit, prange
 
-    def f2(inpt, thrs, gamma, lmbda):
-        return lmbda / (1.0 + np.exp(-gamma * (inpt - thrs)))
+    # raise ImportError
+    log.info("Using numba for parallelizable functions")
 
-    dt = 0.01
-    prob = 1.0 - np.exp(-dt * (1.0 - f2(src_resources, 0.5, 40.0, 1.0)))
+    try:
+        from numba.typed import List
+    except:
+        # older numba versions dont have this
+        def List(*args):
+            return list(*args)
 
-    return prob
+    # silence deprications
+    try:
+        from numba.core.errors import (
+            NumbaDeprecationWarning,
+            NumbaPendingDeprecationWarning,
+        )
+
+        warnings.simplefilter("ignore", category=NumbaDeprecationWarning)
+        warnings.simplefilter("ignore", category=NumbaPendingDeprecationWarning)
+    except:
+        pass
+
+except ImportError:
+    log.info("Numba not available, skipping compilation")
+    # replace numba functions if numba not available:
+    # we only use jit and prange
+    # helper needed for decorators with kwargs
+    def parametrized(dec):
+        def layer(*args, **kwargs):
+            def repl(f):
+                return dec(f, *args, **kwargs)
+
+            return repl
+
+        return layer
+
+    @parametrized
+    def jit(func, **kwargs):
+        return func
+
+    def prange(*args):
+        return range(*args)
+
+    def List(*args):
+        return list(*args)
 
 
 # fmt:off
+@jit(nopython=True, parallel=False, fastmath=False, cache=True)
 def simulate_model(
     tf,
-    output_filename,
     no_gates    = False,
     m0          = 2.0,
     tc          = 45.0,
@@ -108,6 +145,12 @@ def simulate_model(
     rseed : float, optional
         Use a custom random seed to ensure reproducitibility. If None (default), will use whatever Numpy selects
 
+    # Returns
+
+    activity : 2d array, timeseries of module rate. Shape: (n_module, n_timepoints)
+
+    resources : 2d array, timeseries of module resources. Shape: (n_module, n_timepoints)
+
     """
 
     #Set random seed
@@ -121,16 +164,16 @@ def simulate_model(
     nt = int(tf / dt)
 
     # Initialize variables
-    x = np.empty((4, nt))     #activity
-    mvar = np.empty((4, nt))  #resources
-    gate = np.ones((4, 4), dtype=int)  # state of each gate at this time (directed) gate[from, to]
+    x = np.ones(shape=(4, nt), dtype="float")*np.nan     #activity
+    mvar = np.ones(shape=(4, nt), dtype="float")*np.nan  #resources
+    gate = np.ones(shape=(4, 4), dtype="int")  # state of each gate at this time (directed) gate[from, to]
 
 
     # Model parameters!
-    basefiring = np.ones(4) * basefiring  # Minimum (small) firing rate
+    # basefiring = np.ones(4) * basefiring  # Minimum (small) firing rate
 
     # Coupling matrix
-    w = np.zeros((4, 4), dtype=int)  # Adjacency matrix
+    w = np.zeros(shape=(4, 4), dtype="int")  # Adjacency matrix
     w[0, 1] = 1
     w[1, 0] = 1
     w[0, 2] = 1
@@ -150,15 +193,7 @@ def simulate_model(
     # Sigmoid definition (Wilson-Cowan type, such that f(x<=0)=0 and f(+infty)=gain)
     thrsig = np.exp(ksig * basethr)  # Shortcut to make computation faster
 
-    def f(inpt):
-        expinpt = np.exp(-ksig * (inpt - basethr))
-        return (
-            gain * (1.0 - expinpt) / (thrsig * expinpt + 1.0) if inpt >= basethr else 0.0
-        )
 
-    #Sigmoid for gate response
-    def gate_sigm(inpt, thrs, gamma, lmbda):
-        return lmbda / (1.0 + np.exp(-gamma * (inpt - thrs)))
 
     # Initialize parameters to spontaneous, get initial value of resources
     m = np.ones(4) * m0
@@ -182,40 +217,42 @@ def simulate_model(
         old_gate = gate.copy()
         for c in range(4):
 
-            suma = 0.0
+            module_input = 0.0
             # Interaction with other connected clusters (usually one keeps a list of neighbours, etc. but we only have 4 clusters...)
             for neigh in range(4):
                 if w[neigh, c] > 0.0:
 
                     #Sum input to cluster c, only through open gates
-                    suma += (
-                        ((old_gate[neigh, c] == GATE_OPEN) or no_gates) * w0 * x[neigh, j]
-                    )
+                    if (no_gates or (old_gate[neigh, c] == GATE_OPEN)):
+                        module_input += w0 * x[neigh, j]
+
+                    # module_input += (
+                    #     ((old_gate[neigh, c] == GATE_OPEN) or no_gates) * w0 * x[neigh, j]
+                    # )
 
                     # Close door depending on activity of source
                     if old_gate[c, neigh] == GATE_OPEN:
-                        prob = 1.0 - np.exp(
-                            -dt * (1.0 - gate_sigm(mvar[c, j], gatethr, kgate, lamb))
-                        )
+                        prob = probability_to_close(mvar[c, j], dt, gatethr, kgate, lamb)
                         ranumb = np.random.rand()
                         if ranumb < prob:
                             gate[c, neigh] = GATE_CLOSED
+
                     # Open door with a characteristic time
                     else:
                         prob = 1.0 - np.exp(-dt * g)
                         if np.random.rand() < prob:
                             gate[c, neigh] = GATE_OPEN
-            suma *= 0.5
+            module_input *= 0.5
 
             # Multiplicative noise (+ extra additive)
             noise = np.random.standard_normal() * sigma
-            if x[c, j] > basefiring[c]:
+            if x[c, j] > basefiring:
                 noise += np.sqrt(x[c, j]) * np.random.standard_normal() * sigma
 
             # Terms for the deterministic system
-            t1 = b * (x[c, j] - basefiring[c])  # Spontaneous decay to small firing
-            #t2 = (1.0 - x[c,j]) * f(mvar[c, j] * (x[c, j] + suma + ext_input[c]))  # Input to the cluster
-            t2 = f(mvar[c, j] * (x[c, j] + suma + ext_input[c]))  # Input to the cluster
+            t1 = b * (x[c, j] - basefiring)  # Spontaneous decay to small firing
+            #t2 = (1.0 - x[c,j]) * f(mvar[c, j] * (x[c, j] + module_input + ext_input[c]))  # Input to the cluster
+            t2 = transfer_function(mvar[c, j] * (x[c, j] + module_input + ext_input[c]), gain, ksig, basethr, thrsig)  # Input to the cluster
 
             # Update our variables
             x[c, j + 1] = x[c, j] + dt * (-t1 + t2) + sqdt * noise
@@ -225,6 +262,48 @@ def simulate_model(
 
         t += dt  # Update the time
 
+    time_axis = np.arange(0, tf, dt)
+    return time_axis, x, mvar
+
+
+@jit(nopython=True, parallel=False, fastmath=False, cache=True)
+def probability_to_close(resources, dt, gatethr, kgate, lamb):
+    return 1.0 - np.exp(-dt * (1.0 - gate_sigm(resources, gatethr, kgate, lamb)))
+
+# this should be the same?
+def gate_deactivation_function(src_resources):
+    """
+    Auxiliar implementation of the response function of the gate to the number of resources. For plotting purposes only.
+
+    #Parameters:
+    src_resources: ndarray
+        Returns the response function of the given array
+    """
+
+    def f2(inpt, thrs, gamma, lmbda):
+        return lmbda / (1.0 + np.exp(-gamma * (inpt - thrs)))
+
+    dt = 0.01
+    prob = 1.0 - np.exp(-dt * (1.0 - f2(src_resources, 0.5, 40.0, 1.0)))
+
+    return prob
+
+#Sigmoid for gate response
+@jit(nopython=True, parallel=False, fastmath=False, cache=True)
+def gate_sigm(inpt, thrs, gamma, lmbda):
+    return lmbda / (1.0 + np.exp(-gamma * (inpt - thrs)))
+
+@jit(nopython=True, parallel=False, fastmath=False, cache=True)
+def transfer_function(inpt, gain, ksig, basethr, thrsig):
+    expinpt = np.exp(-ksig * (inpt - basethr))
+    return (
+        gain * (1.0 - expinpt) / (thrsig * expinpt + 1.0) if inpt >= basethr else 0.0
+    )
+
+def simulate_and_save(output_filename, **kwargs):
+
+    time, activity, resources = simulate_model(**kwargs)
+
     # ------------
     # Output
     # -----------
@@ -232,8 +311,10 @@ def simulate_model(
 
     # Create a DataFrame easy to read in our workflow and export as HDF
     df = pd.DataFrame(columns=["time"] + [f"mod_{m_cd}" for m_cd in range(1, 5)])
-    df["time"] = np.arange(0, tf, dt)
+    df["time"] = time
     for m_cd in range(4):
-        df[f"mod_{m_cd+1}"] = x[m_cd, :]
-        df[f"mod_{m_cd+1}_res"] = mvar[m_cd, :]
+        df[f"mod_{m_cd+1}"] = activity[m_cd, :]
+        df[f"mod_{m_cd+1}_res"] = resources[m_cd, :]
+
+
     df.to_hdf(f"{output_filename}.hdf5", f"/dataframe", complevel=9)
