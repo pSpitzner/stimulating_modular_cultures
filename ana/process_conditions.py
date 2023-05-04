64fa3bd484a9ac567575de15f34b55bba1a852c6
pSpitzner <paul.spitzner@ds.mpg.de>
2023-05-04 13:38:13 +0200

    added analysis for new 2um and 5um setup.

--- ana/process_conditions.py
+++ ana/process_conditions.py
@@ -2,7 +2,7 @@
 # @Author:        F. Paul Spitzner
 # @Email:         paul.spitzner@ds.mpg.de
 # @Created:       2021-10-25 17:28:21
-# @Last Modified: 2023-03-02 13:58:01
+# @Last Modified: 2023-05-02 09:45:04
 # ------------------------------------------------------------------------------ #
 # Analysis script that preprocesses experiments and creates dataframes to compare
 # across condtions. Plots and more detailed analysis are in `paper_plots.py`
@@ -110,6 +110,12 @@ def main():
         conditions["KCl_1b"] = ["1_KCl_0mM", "2_KCl_2mM"]
     elif args.etype == "exp_bic":
         conditions["Bicuculline_1b"] = ["1_spon_Bic_20uM", "2_stim_Bic_20uM"]
+    elif args.etype == "exp_mod_comp":
+        # comparison between different targeted regions
+        conditions["partial_2u"] = ["1_pre", "2_stim2", "3_post", "4_stim1"]
+        conditions["partial_5u"] = ["1_pre", "2_stim2", "3_post", "4_stim1"]
+        conditions["global_2u"] = ["1_pre", "2_stim", "3_post"]
+        conditions["global_5u"] = ["1_pre", "2_stim", "3_post"]
     elif args.etype == "sim":
         # number of axons between modules as layouts
         # first rate gets stimulated "off" value assigned, second becomes "on"
